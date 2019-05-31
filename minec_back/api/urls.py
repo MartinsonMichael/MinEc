d7ccdacf851214170c10b1abba3de2bd741a5698
Martinson Michael <mmartinson@yandex.ru>
2019-05-31 03:00:07 +0300

    add static yandex ip to allowed hosts

--- minec_back/api/urls.py
+++ minec_back/api/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url, include
 from . import ask_dict_sender, apps
-import base_control
+from base_control.views import index
 
 urlpatterns = [
-    url(r'^control/', include('base_control.urls')),
+    url(r'^control/', index),
     url(r'^get_ask_dict$', ask_dict_sender.get_ask_dict, name='api_ask_dict'),
     url(r'^get/file/', apps.sent_q_as_file, name='api_file'),
     url(r'^get', apps.perform_api, name='api'),
