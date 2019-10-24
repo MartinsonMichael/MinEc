d9e448bcf40d06fa677af7b1db808d52437a2cf7
Martinson Michael <mmartinson@yandex.ru>
2019-10-24 13:12:39 +0300

    del database connect

--- minec_back2/dj_project/settings.py
+++ minec_back2/dj_project/settings.py
@@ -79,16 +79,16 @@ WSGI_APPLICATION = 'dj_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'minec_base_3',
-        'USER': 'michael',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'minec_base_3',
+#         'USER': 'michael',
+#         'PASSWORD': '123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 CACHES = {
     "default": {
