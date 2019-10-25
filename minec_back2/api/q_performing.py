7dd07296cc3e5ca0c3fd71cf57f0b3434b392bfd
Martinson Michael <mmartinson@yandex.ru>
2019-10-25 03:35:26 +0300

    make smart limit

--- minec_back2/api/q_performing.py
+++ minec_back2/api/q_performing.py
@@ -236,7 +236,8 @@ def make_query(
             query = query.group_by(*[grouper['column_obj'] for grouper in text_query[GROUP_KEY]])
 
         # FIXME limit for debugging
-        # query = query.limit(50 * 100)
+        if 'file' not in options_dict.keys():
+            query = query.limit(50 * 100)
         return query.all()
 
 
