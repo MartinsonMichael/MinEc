2cb82bae1e9ab1999fdf33442e8dfe712b3c0f29
Martinson Michael <mmartinson@yandex.ru>
2019-12-08 23:23:17 +0300

    fix

--- minec_back2/dbcontroller/q_performing.py
+++ minec_back2/dbcontroller/q_performing.py
@@ -313,12 +313,14 @@ def get_query(options_dict: Dict[str, List[str]], ticket_id: str, file_path: str
         with open(file_path, 'a') as csv_file:
             writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             for i in range(0, MAX_LIMIT * 10**3, MAX_LIMIT):
+                cur_writted_c = cur_writted
                 for line in query.slice(i, i+MAX_LIMIT).all():
                     writer.writerow([serializer(x) for x in line])
                     cur_writted += 1
                     if cur_writted >= query_cnt:
                         break
-                if cur_writted >= query_cnt:
+
+                if cur_writted >= query_cnt or cur_writted == cur_writted_c:
                     break
 
         print(f'file path: {file_path}')
